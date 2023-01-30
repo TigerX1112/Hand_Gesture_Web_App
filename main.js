bd3a2d739c86acc9db9184c888c2d68f483c063a
TigerX <100389933+TigerX1112@users.noreply.github.com>
2023-01-30 17:48:27 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,28 +1,49 @@
-/*Webcam.attach('#camera')
-
-camera = document.getElementById("camera");
-
-Webcam.set({
-    width:300,
-    height:300,
-    img_format:'png',
-    img_quality:90
-});*/
-Webcam.attach('#camera');
-
-camera = document.getElementById("camera");
+prediction1 = "";
+prediction2 = "";
 
 Webcam.set({
-    width:350,
-    height:300,
-    img_format:'png',
-    img_quality:90
+    width: 350,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90
 });
 
-function saveSnapshot() {
-    webcam.snap(function(data_uri){
-        document.getElementById("div2").innerHTML = '<img id="selfie_image" src="'+data_uri+'">';
-    })
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function take_snapshot() {
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="captured_image" src"'+data_uri+'"/>';
+    });
 }
 
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/9KwtJy0XS/model.json');
+console.log('ml5 version: ', ml5.version);
+
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/9KwtJy0XS/model.json", modelLoaded);
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+}
+
+function speak() {
+    var synth = window.speechSynthesis;
+    speak_data1 = "The first prediction is " + prediction1;
+    speak_data2 = "The second prediction is " + prediction2;
+    var utterThis = new SpeechSynthesisUtterance(speak_data1 + speak_data2);
+    utterThis.rate = 0.5;
+    synth.speak(utterThis);
+}
+
+function identifiy() {
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error){
+        console.error(error);
+    } else {
+        console.log(results);
+    }
+}
